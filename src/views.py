60abf080d53c16717d41653c5cb0c71bd78c9340
LisandroLedesma <lichaledesma31@gmail.com>
2025-01-13 23:58:01 -0300

    add cache en consulta

--- src/views.py
+++ src/views.py
@@ -6,9 +6,12 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        options = EcommerceRepository.get_all()
-        return render_template('index.html', options=options)
-    elif request.method == 'POST':
-        print(request.form)
-        pass
+    options = EcommerceRepository.get_all()    
+    if request.method == 'POST': 
+        search_query = request.form.get('search_query') 
+        category = request.form.get('category') 
+    else: 
+        search_query = ""
+        category = ""
+
+    return render_template('index.html', search_query=search_query, category=category, options=options)
